f7ccc2696e3c897725a4e61f173b0a079777da08
akorelyakov <al.korelyakov@gmail.com>
2020-06-12 12:21:23 +0300

    add ResumeServlet

---
+++ src/com/github/akorelyakov/webapp/web/ResumeServlet.java
@@ -0,0 +1,21 @@
+package com.github.akorelyakov.webapp.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ResumeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        String name = request.getParameter("uuid");
+        response.getWriter().write(name == null ? "Hello!" : "Hello!" + name);
+    }
+}

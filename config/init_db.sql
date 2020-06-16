84724ae93a712f33d66089eb245222375f92582c
akorelyakov <al.korelyakov@gmail.com>
2020-06-16 10:11:15 +0300

    add init_db.sql

--- config/init_db.sql
+++ config/init_db.sql
@@ -10,4 +10,14 @@ CREATE TABLE contact (
   value       TEXT     NOT NULL
 );
 CREATE UNIQUE INDEX contact_uuid_type_index
-  ON contact (resume_uuid, type);
+    ON contact (resume_uuid, type);
+
+CREATE TABLE section (
+    id SERIAL,
+    resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+    type TEXT NOT NULL,
+    value TEXT NOT NULL
+);
+CREATE UNIQUE INDEX section_index
+    ON section (resume_uuid, type);
+
